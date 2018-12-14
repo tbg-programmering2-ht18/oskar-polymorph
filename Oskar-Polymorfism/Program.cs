4367b1ab92992cef99bea58b188125be07a19dc0
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-12-14 13:06:36 +0100

    Test
    Trying to commit

---
+++ Oskar-Polymorfism/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oskar_Polymorfism
+{
+    class Player
+    {
+        public virtual void Attack()
+        {
+            Console.WriteLine("A Player attacks!");
+        }
+    }
+    //test test
+    class Mage : Player
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("A Mage attacks with a fireball!");
+        }
+    }
+
+    class Warrior : Player
+    {
+        public override void Attack()
+        {
+            Console.WriteLine("A Warrior attacks with a sword!");
+        }
+    }
+    //testing
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            players.Add(new Player());
+            players.Add(new Mage());
+            players.Add(new Warrior());
+
+            foreach (Player p in players)
+            {
+                p.Attack();
+            }
+            Console.ReadKey();
+        }
+    }
+}
